885b108f876523811085bc9d22fd632117bdca1f
Vlad Vyshnevskyi <schnewsk@gmail.com>
2020-07-07 16:25:37 +0300

    Initial Commit

---
+++ Money App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Money App
+//
+//  Created by Дмитрий Котырло on 07.07.2020.
+//  Copyright © 2020 _vyshnevskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
